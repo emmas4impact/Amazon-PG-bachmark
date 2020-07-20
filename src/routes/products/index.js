b37e0136cf47cc5232773c983618e769005ab445
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-20 10:30:57 +0300

    Update index.js

--- src/routes/products/index.js
+++ src/routes/products/index.js
@@ -2,14 +2,82 @@ const express = require("express")
 const db = require("../../db")
 
 const router = express.Router()
+const fs = require("fs-extra")
+const path = require("path")
+const multer = require("multer")
+const upload = multer()
+const port = process.env.PORT
+const imagePath = path.join(__dirname, "../../../public/img/products")
+console.log(imagePath)
+
+router.post("/:id/upload", upload.single("product"), async (req, res, next) => {
+    try {
+      await fs.writeFile(path.join(imagePath, `${req.params.id}.png`), req.file.buffer)
+      req.body = {
+        imageurl: `http://127.0.0.1:${port}/img/products/${req.params.id}.png`
+      }
+      let params = []
+      let query = 'UPDATE "products" SET '
+      for (bodyParamName in req.body) {
+          query += // for each element in the body I'll add something like parameterName = $Position
+              (params.length > 0 ? ", " : '') + //I'll add a coma before the parameterName for every parameter but the first
+              bodyParamName + " = $" + (params.length + 1) // += Category = $1 
+          params.push(req.body[bodyParamName]) //save the current body parameter into the params array
+      }
+      params.push(req.params.id) //push the id into the array
+      query += " WHERE _id = $" + (params.length) + " RETURNING *" 
+      console.log(query)
+      const result = await db.query(query, params) //querying the DB for updating the row
+      if (result.rowCount === 0) //if no element match the specified id => 404
+          return res.status(404).send("Not Found")
+      res.send(result.rows[0]) //else, return the updated version
+    } catch (error) {
+      next(error)
+    }
+  })
 
 
 router.get("/", async(req, res)=>{
+   // const order = req.query.order || "asc"
+    const offset = req.query.offset || 0
+    const limit = req.query.limit
     
-    const response = await db.query('SELECT * FROM "products"')
-    res.send(response.rows)
+    //delete req.query.order
+    delete req.query.offset
+    delete req.query.limit
+    
+    let query = 'SELECT * FROM "products"'
+    const params=[]
+    for (const queryParam in req.query) {
+        params.push(req.query[queryParam])
+        if (params.length === 1) 
+            query += `WHERE ${queryParam} = $${params.length} `
+        else 
+            query += ` AND ${queryParam} = $${params.length} `
+    }
+    
+    //query += " ORDER BY brand " + order; 
+
+    params.push (limit)
+    query += ` LIMIT $${params.length} `
+    params.push(offset)
+    query += ` OFFSET $${params.length}`
+    
+    
+    const response = await db.query(query, params)
+    res.send({count: response.rows.length, products: response.rows})
+
 })
 
+router.get("/search/:query", async(req, res) => {
+    const response = await db.query(`SELECT * FROM "products" WHERE 
+                                    brand ILIKE '${"%" + req.params.query + "%"}' OR
+                                    category ILIKE '${"%" + req.params.query + "%"}'
+                                    LIMIT $1 OFFSET $2 
+                                    `, [ req.query.limit || 10, req.query.offset || 0])
+
+    res.send(response.rows)
+})
 router.get("/:id", async (req, res)=>{
     const response = await db.query('SELECT * FROM "products" WHERE _id= $1', 
                                                                                         [ req.params.id ])
@@ -20,11 +88,22 @@ router.get("/:id", async (req, res)=>{
     res.send(response.rows[0])
 })
 
+router.get("/:id/reviews", async (req, res)=>{
+    const response = await db.query(`SELECT  * FROM "reviews" 
+                                    JOIN "products" ON reviews.productid= products._id 
+                                    AND products._id = $1`, 
+                                    [req.params.id])
+    if (response.rowCount === 0) 
+        return res.status(404).send("Not found")
+
+    res.send(response.rows)
+})
+
 router.post("/", async (req, res)=> {
-    const response = await db.query(`INSERT INTO "products" ( projectname, description, repourl, liveurl, studentid ) 
-                                     Values ($1, $2, $3, $4, $5)
+    const response = await db.query(`INSERT INTO "products" ( product_name, brand, description, imageurl, category, price) 
+                                     Values ($1, $2, $3, $4, $5,$6)
                                      RETURNING *`, 
-                                    [ req.body.projectname, req.body.description, req.body.repourl, req.body.liveurl, req.body.studentid ])
+                                    [ req.body.product_name, req.body.brand, req.body.description, req.body.imageurl, req.body.category, req.body.price ])
     
     console.log(response)
     res.send(response.rows[0])
@@ -43,7 +122,7 @@ router.put("/:id", async (req, res)=> {
         }
 
         params.push(req.params.id) //push the asin into the array
-        query += " WHERE _id = $" + (params.length) + " RETURNING *" //adding filtering for ASIN + returning
+        query += " WHERE _id = $" + (params.length) + " RETURNING *" 
         console.log(query)
 
         const result = await db.query(query, params) //querying the DB for updating the row
